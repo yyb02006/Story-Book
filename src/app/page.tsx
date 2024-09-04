a52f3be8af95390709ab347366e245047345a2cc
alwjrqns <yyb02006@gmail.com>
2024-09-04 23:30:30 +0900

    Feat : story 라우트 생성 및 루트 페이지 링크 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,10 @@
+import Link from 'next/link'
+
 export default function Home() {
-  return <div></div>
+  return (
+    <div>
+      <div>Story Book</div>
+      <Link href="/story">나만의 이야기 만들기</Link>
+    </div>
+  )
 }
---
+++ src/app/story/page.tsx
@@ -0,0 +1,3 @@
+export default function StoryPage() {
+  return <div>나의 이야기</div>
+}
