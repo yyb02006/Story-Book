290bc36873cb33fcdd69aa1287d91cfaf5f3f2e9
alwjrqns <yyb02006@gmail.com>
2024-10-28 23:58:48 +0900

    Feat : 글로벌 네비게이션 컴포넌트 추가

---
+++ src/components/nav/globalNav.tsx
@@ -0,0 +1,10 @@
+export default function GlobalNav() {
+  return (
+    <nav className="fixed h-screen w-20">
+      <ul>
+        <h1>myname</h1>
+        <li></li>
+      </ul>
+    </nav>
+  )
+}
