712c40719f5a8646451231a1413061ce67768f28
alwjrqns <yyb02006@gmail.com>
2024-11-03 18:58:26 +0900

    Feat : 세 개로 겹치는 wave 애니메이션 구현 및 waveConfig 추가

--- src/components/waveCanvas.tsx
+++ src/components/waveCanvas.tsx
@@ -13,7 +13,12 @@ interface Point {
 
 export default function WaveCanvas() {
   const canvasRef = useRef<HTMLCanvasElement>(null)
-  const pointConfig = { pointCount: 10, pointSpeed: 0.05 }
+  const waveConfig = {
+    width: 30,
+    colors: ['blue', 'crimson', 'purple'],
+    pointCount: 20,
+    speed: 0.05,
+  }
 
   const clearCanvas = (ctx: CanvasRenderingContext2D) => {
     ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height)
@@ -23,13 +28,13 @@ export default function WaveCanvas() {
     return { x, y, initialX: x, radius: 20, phaseX: 0, phaseShift }
   }
 
-  const updatePoints = (points: Point[]) => {
-    points.forEach((point, index, originalArray) => {
+  const updateWaves = (wave: Point[]) => {
+    wave.forEach((point, index, originalArray) => {
       if (index > 0 && index < originalArray.length - 1) {
         const { initialX, phaseShift } = point
         const offset = initialX
         const amplitudeScale = initialX * Math.sin(point.phaseX + phaseShift)
-        point.phaseX += pointConfig.pointSpeed
+        point.phaseX += waveConfig.speed
         point.x = offset + amplitudeScale
       }
     })
@@ -37,15 +42,63 @@ export default function WaveCanvas() {
 
   // quadraticCurveTo 작동 원리 알아보기 (이전 점과 현재 점의 평균 좌표를 넘겨줘야하는 이유에 대해)
   // resize시 곡선도 따라가거나 안따라가더라도 찌그러지지 않도록 기능 추가 필요
-  const drawPoints = (ctx: CanvasRenderingContext2D, canvasHeight: number, points: Point[]) => {
-    const [firstPoint, lastPoint] = [points[0], points[points.length - 1]]
+  const drawWave = (
+    ctx: CanvasRenderingContext2D,
+    canvasHeight: number,
+    wave: Point[],
+    color: string,
+    // waveCount: number,
+  ) => {
+    const [firstPoint, lastPoint] = [wave[0], wave[wave.length - 1]]
+
+    /*
+    하나의 웨이브를 기준으로 여러개의 다른 웨이브를 생성하는 방법
+    
+    const getShiftedPoint = (point: Point, waveIdx: number) => {
+      const currentOffset = point.initialX
+      const shiftFactor = (2 * Math.PI * waveIdx) / waveCount
+      const currentAmplitudeScale =
+        point.initialX * Math.sin(point.phaseX + point.phaseShift + shiftFactor)
+      return currentOffset + currentAmplitudeScale
+    } 
+      
+    Array.from({ length: waveCount }).forEach((_, waveIdx) => {
+      ctx.beginPath()
+      ctx.moveTo(firstPoint.x, firstPoint.y)
+
+      wave.forEach((point, pointIdx, originalArray) => {
+        if (pointIdx === 0) return
+        const currentX = getShiftedPoint(point, waveIdx)
+
+        const prevPoint = originalArray[pointIdx - 1]
+        const prevX = getShiftedPoint(prevPoint, waveIdx)
+
+        ctx.quadraticCurveTo(
+          prevX,
+          prevPoint.y,
+          (prevX + currentX) / 2,
+          (prevPoint.y + point.y) / 2,
+        )
+
+        // 마지막 point 추가
+        if (pointIdx === originalArray.length - 1) ctx.lineTo(lastPoint.x, lastPoint.y)
+      })
+
+      ctx.lineTo(0, canvasHeight)
+      ctx.lineTo(0, 0)
+      ctx.fillStyle = 'blue'
+      ctx.fill()
+      ctx.closePath()
+    })
+    */
 
     ctx.beginPath()
     ctx.moveTo(firstPoint.x, firstPoint.y)
 
-    points.forEach((point, index, originalArray) => {
-      if (index === 0) return
-      const prevPoint = originalArray[index - 1]
+    wave.forEach((point, pointIdx, originalArray) => {
+      if (pointIdx === 0) return
+      const prevPoint = originalArray[pointIdx - 1]
+
       ctx.quadraticCurveTo(
         prevPoint.x,
         prevPoint.y,
@@ -54,12 +107,13 @@ export default function WaveCanvas() {
       )
 
       // 마지막 point 추가
-      if (index === originalArray.length - 1) ctx.lineTo(lastPoint.x, lastPoint.y)
+      if (pointIdx === originalArray.length - 1) ctx.lineTo(lastPoint.x, lastPoint.y)
     })
 
     ctx.lineTo(0, canvasHeight)
     ctx.lineTo(0, 0)
-    ctx.fillStyle = 'blue'
+    ctx.fillStyle = color
+    ctx.globalAlpha = 0.3
     ctx.fill()
     ctx.closePath()
   }
@@ -70,14 +124,27 @@ export default function WaveCanvas() {
     if (!ctx || !canvas) return
     canvas.height = window.innerHeight
 
-    const points = Array.from({ length: pointConfig.pointCount }).map((_, index, originalArray) =>
-      createPoint(canvas.width / 2, (canvas.height * index) / (originalArray.length - 1), index),
-    )
+    // 여러개의 웨이브를 각자 생성하는 방법
+    const waves = waveConfig.colors.map((color, waveIdx, originalArray) => {
+      const shiftFactor = (2 * Math.PI * waveIdx) / (originalArray.length * 2)
+      return {
+        color,
+        wave: Array.from({ length: waveConfig.pointCount }, (_, pointIdx) =>
+          createPoint(
+            canvas.width / 2,
+            (canvas.height * pointIdx) / (waveConfig.pointCount - 1),
+            pointIdx + shiftFactor,
+          ),
+        ),
+      }
+    })
 
     const animate = () => {
       clearCanvas(ctx)
-      drawPoints(ctx, canvas.height, points)
-      updatePoints(points)
+      waves.forEach(({ color, wave }) => {
+        drawWave(ctx, canvas.height, wave, color)
+        updateWaves(wave)
+      })
       requestAnimationFrame(animate)
     }
     animate()
@@ -94,10 +161,10 @@ export default function WaveCanvas() {
 
   return (
     <canvas
-      className="relative bg-red-500"
+      className="relative"
       ref={canvasRef}
-      width={480}
+      width={waveConfig.width}
       height={canvasRef.current?.height}
-    ></canvas>
+    />
   )
 }
