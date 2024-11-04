93cbdd3d981e7c4a82d200df07c2a50a93f36668
alwjrqns <yyb02006@gmail.com>
2024-11-04 23:45:50 +0900

    Feat : GNB 트랜지션 추가

--- src/components/nav/globalNav.tsx
+++ src/components/nav/globalNav.tsx
@@ -1,15 +1,23 @@
-import WaveCanvas from '#/components/waveCanvas'
+'use client'
+
+import { WaveWithInitAnim } from '#/components/waveCanvas'
 import { cls } from '#/libs/client/utils'
+import { motion } from 'framer-motion'
 
 export default function GlobalNav() {
   const wave = {
     colors: ['rgba(29, 42, 120, 0.8)', 'rgba(44, 62, 80, 0.3)', 'rgba(236, 240, 241, 0.3)'],
-    width: 40,
+    width: 20,
     pointCountEachWave: 60,
     speed: 0.05,
   }
   return (
-    <nav className="fixed h-screen w-20">
+    <motion.nav
+      initial={{ x: -10, opacity: 0 }}
+      animate={{ x: 0, opacity: 1 }}
+      transition={{ ease: 'easeOut', duration: 0.2 }}
+      className="fixed h-screen w-20"
+    >
       {wave.colors.map((color) => (
         <div key={color} className={cls('absolute size-full')} style={{ backgroundColor: color }} />
       ))}
@@ -17,9 +25,7 @@ export default function GlobalNav() {
         <h1>myname</h1>
         <li></li>
       </ul>
-      <div className={cls('absolute top-0')} style={{ right: -wave.width }}>
-        <WaveCanvas {...wave} />
-      </div>
-    </nav>
+      <WaveWithInitAnim {...wave} />
+    </motion.nav>
   )
 }
