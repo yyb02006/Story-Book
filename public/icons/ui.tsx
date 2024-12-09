a13718f76eacd4659b8e5022cb04725d374e747e
alwjrqns <yyb02006@gmail.com>
2024-12-10 00:58:43 +0900

    Feat : svg 프로퍼티 수정

--- public/icons/ui.tsx
+++ public/icons/ui.tsx
@@ -1,10 +1,18 @@
-import { SVGProps } from 'react'
+/* eslint-disable tailwindcss/no-custom-classname */
 
-interface IconSvgProps extends SVGProps<SVGSVGElement> {
-  icon_origin_id: string
-}
+type StrokeColor = `#${string}`
 
-const getCustomIcons: IconSvgProps = ({ icon_origin_id, ...props }) => (
+export const Search = ({
+  width,
+  height,
+  strokeWidth = 2,
+  strokeColor = '#eaeaea',
+}: {
+  width: `${number}`
+  height: `${number}`
+  strokeWidth?: number
+  strokeColor?: StrokeColor
+}) => (
   <svg
     data-icon-name="search-alt"
     data-style="line"
@@ -12,12 +20,18 @@ const getCustomIcons: IconSvgProps = ({ icon_origin_id, ...props }) => (
     viewBox="0 0 24 24"
     xmlns="http://www.w3.org/2000/svg"
     id="search-alt"
-    class="icon line"
-    width="48"
-    height="48"
+    className="icon line"
+    width={width}
+    height={height}
   >
     <path
-      style={{ fill: 'none', stroke: 'rgb(0, 0, 0)' }}
+      style={{
+        fill: 'none',
+        stroke: strokeColor,
+        strokeLinecap: 'round',
+        strokeLinejoin: 'round',
+        strokeWidth: strokeWidth,
+      }}
       d="M18,10.5A7.5,7.5,0,1,1,10.5,3,7.5,7.5,0,0,1,18,10.5ZM21,21l-5.2-5.2"
       id="primary"
     ></path>
