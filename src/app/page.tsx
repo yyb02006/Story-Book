953645fe24bbcbdc0b4db270b49cf300209ba8d0
alwjrqns <yyb02006@gmail.com>
2024-12-16 23:58:08 +0900

    Feat : 메인 페이지 레이아웃 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,12 @@ import Link from 'next/link'
 
 export default function Home() {
   return (
-    <section className="h-screen">
-      <h1>Story Book</h1>
-      <Link href="/story">나만의 이야기 만들기</Link>
-    </section>
+    <div className="flex h-screen w-full flex-col">
+      <div className="flex w-full justify-center bg-sky-500">
+        <div className="h-[30px] w-[400px] bg-purple-500">검색</div>
+        <Link href="/story">나만의 이야기 만들기</Link>
+      </div>
+      <div className="size-full bg-orange-400">notes</div>
+    </div>
   )
 }
