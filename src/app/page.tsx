8a8e44466ca161b145b5509c11a7780bd4bb1296
alwjrqns <yyb02006@gmail.com>
2024-12-23 22:05:16 +0900

    Feat : SearchForm 적용 및 메인 페이지 CSR전환

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
+'use client'
+
+import SearchForm from '#/components/searchForm'
 import Link from 'next/link'
 
 export default function Home() {
   return (
     <div className="flex h-screen w-full flex-col">
-      <div className="flex w-full justify-center bg-sky-500">
-        <div className="h-[30px] w-[400px] bg-purple-500">검색</div>
+      <div className="flex w-full justify-center">
+        <SearchForm onSearch={() => {}} setSearchWord={() => {}} searchWord="" />
         <Link href="/story">나만의 이야기 만들기</Link>
       </div>
       <div className="size-full bg-orange-400">notes</div>
