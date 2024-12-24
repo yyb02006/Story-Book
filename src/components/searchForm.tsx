1d4fcd7c51a87367070e26a54d0b5e0e2e2a1b7a
alwjrqns <yyb02006@gmail.com>
2024-12-24 23:59:31 +0900

    Feat : 서치 바 레이아웃 수정

--- src/components/searchForm.tsx
+++ src/components/searchForm.tsx
@@ -9,8 +9,24 @@ interface SearchFormProps {
 
 export default function SearchForm({ onSearch, setSearchWord, searchWord }: SearchFormProps) {
   return (
-    <form onSubmit={onSearch} className="flex space-x-2">
-      <button type="submit">
+    <form onSubmit={onSearch} className="flex h-8 space-x-2">
+      <div className="flex rounded-full bg-red-500">
+        <div className="aspect-square rounded-s-full bg-amber-500"></div>
+        <Input
+          name="search"
+          type="text"
+          placeholder="search"
+          css="border-none placeholder:font-bold bg-transparent px-2 rounded-full"
+          onChange={(e: SyntheticEvent<HTMLInputElement>) => {
+            setSearchWord(e.currentTarget.value)
+          }}
+          value={searchWord}
+        />
+      </div>
+      <button
+        type="submit"
+        className="flex aspect-square h-full items-center justify-center rounded-full bg-pink-500"
+      >
         <svg
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
@@ -26,16 +42,6 @@ export default function SearchForm({ onSearch, setSearchWord, searchWord }: Sear
           />
         </svg>
       </button>
-      <Input
-        name="search"
-        type="text"
-        placeholder="search"
-        css="border-none placeholder:font-bold bg-transparent"
-        onChange={(e: SyntheticEvent<HTMLInputElement>) => {
-          setSearchWord(e.currentTarget.value)
-        }}
-        value={searchWord}
-      />
     </form>
   )
 }
