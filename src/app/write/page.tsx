0cf25dce9f1209fbd8e77bc938ea0b065308000c
alwjrqns <yyb02006@gmail.com>
2025-01-01 23:47:08 +0900

    Feat : write 경로 생성

---
+++ src/app/write/page.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+import { Editor } from '#/libs/client/Editor'
+
+export default function Write() {
+  return (
+    <div className="h-screen w-full">
+      <Editor />
+    </div>
+  )
+}
