7dbc5b1e9de8b36126152a607cebf84347ccf3e9
alwjrqns <yyb02006@gmail.com>
2025-01-07 23:52:00 +0900

    Feat : lexical Editor 컴포넌트 기본 설정

---
+++ src/libs/client/Editor.tsx
@@ -0,0 +1,43 @@
+import { AutoFocusPlugin } from '@lexical/react/LexicalAutoFocusPlugin'
+import { LexicalComposer } from '@lexical/react/LexicalComposer'
+import { RichTextPlugin } from '@lexical/react/LexicalRichTextPlugin'
+import { ContentEditable } from '@lexical/react/LexicalContentEditable'
+import { HistoryPlugin } from '@lexical/react/LexicalHistoryPlugin'
+import { LexicalErrorBoundary } from '@lexical/react/LexicalErrorBoundary'
+import { ComponentProps } from 'react'
+import { nodes } from '#/libs/client/nodes'
+
+const theme = {
+  // Theme styling goes here
+  //...
+}
+
+// Catch any errors that occur during Lexical updates and log them
+// or throw them as needed. If you don't throw them, Lexical will
+// try to recover gracefully without losing user data.
+function onError(error: unknown) {
+  console.error(error)
+}
+
+export function Editor() {
+  const initialConfig: ComponentProps<typeof LexicalComposer>['initialConfig'] = {
+    namespace: 'MyEditor',
+    theme,
+    onError,
+    nodes: nodes,
+  }
+
+  return (
+    <LexicalComposer initialConfig={initialConfig}>
+      <div className="relative">
+        <RichTextPlugin
+          contentEditable={<ContentEditable />}
+          placeholder={<div className="absolute left-0 top-0">Enter some text...</div>}
+          ErrorBoundary={LexicalErrorBoundary}
+        />
+      </div>
+      <HistoryPlugin />
+      <AutoFocusPlugin />
+    </LexicalComposer>
+  )
+}
