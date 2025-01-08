be4177e8f0cbc9a25ed71d731c9c77be7f5d5ec5
alwjrqns <yyb02006@gmail.com>
2025-01-08 23:57:59 +0900

    Feat : searchForm 컴포넌트의 svg를 외부 컴포넌트로부터 불러오도록 수정

--- src/components/searchForm.tsx
+++ src/components/searchForm.tsx
@@ -19,11 +19,11 @@ export default function SearchForm({
     <form onSubmit={onSearch} className="flex h-14 space-x-2">
       <div className="flex rounded-full bg-[#202020] ring-1 ring-[#707070]" style={{ width: w }}>
         <button className="flex items-center p-2">
-          <div className="mr-1 flex aspect-square h-full items-center justify-center rounded-full bg-sky-400">
+          <div className="mr-1 flex aspect-square h-full items-center justify-center rounded-full border border-[#707070]">
             <Search width={20} height={20} strokeColor="#eaeaea" strokeWidth={3} />
           </div>
           <div className="flex items-center">
-            <div className="mt-[2px] border-4 border-transparent border-t-purple-300"></div>
+            <div className="mt-[2px] border-4 border-transparent border-t-smooth-white"></div>
           </div>
         </button>
         <Input
@@ -39,20 +39,7 @@ export default function SearchForm({
       </div>
       <button type="submit" className="flex aspect-square items-center justify-center p-1">
         <div className="flex aspect-square h-full items-center justify-center rounded-full bg-[#516afc]">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={2}
-            stroke="currentColor"
-            className="size-6"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
-            />
-          </svg>
+          <Search width={24} height={24} strokeColor="#eaeaea" strokeWidth={2} />
         </div>
       </button>
     </form>
