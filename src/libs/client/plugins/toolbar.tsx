b544c0f57830d79e90cfc0fde4104b56a9ae12d0
alwjrqns <yyb02006@gmail.com>
2025-01-09 23:43:28 +0900

    Feat : 툴바 플러그인 추가

---
+++ src/libs/client/plugins/toolbar.tsx
@@ -0,0 +1,11 @@
+const SupportedBlockType = {
+  paragraph: 'Paragraph',
+  h1: 'Heading 1',
+  h2: 'Heading 2',
+  h3: 'Heading 3',
+  h4: 'Heading 4',
+  h5: 'Heading 5',
+  h6: 'Heading 6',
+} as const
+
+type BlockType = keyof typeof SupportedBlockType
