386d42bca545c83a52ffb9e7db38faaba136c70c
alwjrqns <yyb02006@gmail.com>
2025-01-10 23:58:51 +0900

    Feat : toolbar 실제 버튼 요소 추가

--- src/libs/client/plugins/toolbar.tsx
+++ src/libs/client/plugins/toolbar.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react'
+
 const SupportedBlockType = {
   paragraph: 'Paragraph',
   h1: 'Heading 1',
@@ -9,3 +11,20 @@ const SupportedBlockType = {
 } as const
 
 type BlockType = keyof typeof SupportedBlockType
+
+export const Toolbar = () => {
+  const [blockType, setBlockType] = useState<BlockType>('paragraph')
+  return (
+    <div>
+      <button
+        type="button"
+        role="checkbox"
+        title={SupportedBlockType['h1']}
+        aria-label={SupportedBlockType['h1']}
+        aria-checked={blockType === 'h1'}
+      >
+        H1
+      </button>
+    </div>
+  )
+}
