b679af1af57a82e278d110c7d3387553d3e692dc
alwjrqns <yyb02006@gmail.com>
2025-01-20 23:51:34 +0900

    Feat : 첫 글자를 대문자로 바꾸는 유틸함수 추가

--- src/libs/client/utils.ts
+++ src/libs/client/utils.ts
@@ -13,3 +13,19 @@
 export function cls(...className: string[]) {
   return className.join(' ')
 }
+
+/**
+ *
+ * 주어진 문자열의 첫 글자를 대문자로 변환후 반환
+ *
+ * @param {string} string - 변환할 문자열
+ * @returns {string} 첫 글자가 대문자로 변환된 문자열
+ *
+ * @example
+ * const modifiedString = ('first-Letter');
+ *
+ * console.log(modifiedString); // "First-Letter"
+ */
+export function cfl(string: string) {
+  return string ? string.charAt(0).toUpperCase() + string.slice(1) : string
+}
