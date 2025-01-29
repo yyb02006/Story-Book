3951b67c8a9106b5ed55c660b011aea561f90664
alwjrqns <yyb02006@gmail.com>
2025-01-29 23:56:13 +0900

    Feat : theme 객체를 생성하는 모듈 추가

---
+++ src/components/editorTheme.ts
@@ -0,0 +1,6 @@
+import { EditorThemeClasses } from 'lexical'
+import editorTheme from '#/components/editorTheme.module.css'
+
+export const theme: EditorThemeClasses = {
+  quote: editorTheme.quote,
+}
