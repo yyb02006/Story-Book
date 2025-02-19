8d5423aff77b62cad9cfa30b10c15bec02a8cfda
alwjrqns <yyb02006@gmail.com>
2025-02-19 23:32:17 +0900

    Feat : editorTheme.ts에 리스트 스타일 추가

--- src/components/editorTheme.ts
+++ src/components/editorTheme.ts
@@ -3,4 +3,9 @@ import editorTheme from '#/components/editorTheme.module.css'
 
 export const theme: EditorThemeClasses = {
   quote: editorTheme.quote,
+  list: {
+    ol: editorTheme.ol,
+    ul: editorTheme.ul,
+    listitem: editorTheme.li,
+  },
 }
