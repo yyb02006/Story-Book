78b9cfbd7a07b44d6184c6b5a6b5a0469fbe3d5f
alwjrqns <yyb02006@gmail.com>
2025-02-20 23:52:54 +0900

    Style : 에디터의 heading 요소에 대한 스타일을 theme 프로퍼티로 이동

--- src/components/editorTheme.module.css.d.ts
+++ src/components/editorTheme.module.css.d.ts
@@ -1,4 +1,8 @@
 declare const styles: {
+  readonly h1: string
+  readonly h2: string
+  readonly h3: string
+  readonly h4: string
   readonly quote: string
   readonly ol: string
   readonly ul: string
--- src/components/editorTheme.ts
+++ src/components/editorTheme.ts
@@ -2,6 +2,12 @@ import { EditorThemeClasses } from 'lexical'
 import editorTheme from '#/components/editorTheme.module.css'
 
 export const theme: EditorThemeClasses = {
+  heading: {
+    h1: editorTheme.h1,
+    h2: editorTheme.h2,
+    h3: editorTheme.h3,
+    h4: editorTheme.h4,
+  },
   quote: editorTheme.quote,
   list: {
     ol: editorTheme.ol,
