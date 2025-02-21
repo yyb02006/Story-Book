0c988f71240d8b109867065677398b7deae8b961
alwjrqns <yyb02006@gmail.com>
2025-02-21 23:55:02 +0900

    Feat : 코드 블럭 구현을 위한 코드노드 추가

--- src/libs/client/nodes.ts
+++ src/libs/client/nodes.ts
@@ -1,5 +1,13 @@
 import { HeadingNode, QuoteNode } from '@lexical/rich-text'
 import { ListItemNode, ListNode } from '@lexical/list'
 import { Klass, LexicalNode } from 'lexical'
+import { CodeNode, CodeHighlightNode } from '@lexical/code'
 
-export const nodes: Klass<LexicalNode>[] = [HeadingNode, QuoteNode, ListNode, ListItemNode]
+export const nodes: Klass<LexicalNode>[] = [
+  HeadingNode,
+  QuoteNode,
+  ListNode,
+  ListItemNode,
+  CodeNode,
+  CodeHighlightNode,
+]
