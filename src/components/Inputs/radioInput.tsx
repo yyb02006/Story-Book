42d1aa160af262b16b1c5f5b93d1f071733b817f
alwjrqns <yyb02006@gmail.com>
2025-04-16 04:21:30 +0900

    Refactor : radioInput 책임 분리 및 리팩토링

---
+++ src/components/Inputs/radioInput.tsx
@@ -0,0 +1,52 @@
+import { cls } from '#/libs/client/utils'
+import { ChangeEvent, InputHTMLAttributes } from 'react'
+
+interface InputProps
+  extends Omit<InputHTMLAttributes<HTMLInputElement>, 'value' | 'onChange' | 'name'> {
+  name: string
+  onChange?: (value: string, event: ChangeEvent<HTMLInputElement>) => void
+  value?: string | undefined
+  radioId?: string
+  className?: string
+  peerClassName?: string
+  labelName?: string
+  radioDisabled?: boolean
+}
+
+export default function RadioInput({
+  name,
+  onChange,
+  value,
+  radioId,
+  className = '',
+  peerClassName = '',
+  labelName,
+  radioDisabled,
+  ...rest
+}: InputProps) {
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    onChange?.(event.target.value, event)
+  }
+  return (
+    <label className="block h-full cursor-pointer">
+      <input
+        onChange={handleChange}
+        type="radio"
+        id={radioId}
+        name={name}
+        value={value}
+        disabled={radioDisabled}
+        className={cls(className, 'peer hidden')}
+        {...rest}
+      />
+      <div
+        className={cls(
+          peerClassName,
+          'peer-checked:bg-palettered flex h-full items-center justify-center text-[#bababa]',
+        )}
+      >
+        {labelName}
+      </div>
+    </label>
+  )
+}
