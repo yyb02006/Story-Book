51bd8d34a84b4d1dd884af63d5fa3674b2baa0f2
alwjrqns <yyb02006@gmail.com>
2025-04-20 20:55:51 +0900

    Feat : searchBar의 위치를 App 라우터에서 레이아웃으로 이동

--- src/app/write/page.tsx
+++ src/app/write/page.tsx
@@ -2,7 +2,7 @@ import { Editor } from '#/components/editor'
 
 export default function Write() {
   return (
-    <div className="flex h-screen justify-center">
+    <div className="flex h-full justify-center">
       <div className="max-w-[800px] grow">
         <Editor />
       </div>
