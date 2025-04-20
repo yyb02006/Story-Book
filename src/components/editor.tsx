5ee21b0623a4e07ed8a94ab05ce3c6c6f51fb66e
alwjrqns <yyb02006@gmail.com>
2025-04-20 18:14:48 +0900

    Feat : 제목 인풋 추가

--- src/components/editor.tsx
+++ src/components/editor.tsx
@@ -13,22 +13,25 @@ import { theme } from '#/components/editorTheme'
 import { ListPlugin } from '@lexical/react/LexicalListPlugin'
 import { CheckListPlugin } from '@lexical/react/LexicalCheckListPlugin'
 import CodeHighlightPlugin from '#/components/plugins/codeHighlightPlugin'
+import { TextInput } from '#/components/Inputs'
 
 function onError(error: unknown) {
   console.error(error)
 }
 
 const TextEditorContainer = ({ children }: { children: JSX.Element | JSX.Element[] }) => {
-  return <div className="relative flex h-60 flex-col bg-[#303030]">{children}</div>
+  return (
+    <div className="relative flex h-80 flex-col space-y-4 rounded-2xl border border-[#404040] bg-dark-gray p-3">
+      {children}
+    </div>
+  )
 }
 
 const PlaceHolder = ({ children }: { children: ReactNode }) => {
-  return <div className="pointer-events-none absolute left-0 top-0">{children}</div>
+  return <div className="pointer-events-none absolute left-1 top-0">{children}</div>
 }
 
 export function Editor() {
-  console.log(theme)
-
   const initialConfig: ComponentProps<typeof LexicalComposer>['initialConfig'] = {
     namespace: 'MyEditor',
     theme,
@@ -38,6 +41,12 @@ export function Editor() {
 
   return (
     <LexicalComposer initialConfig={initialConfig}>
+      <TextInput
+        name="title"
+        value=""
+        className="h-14 rounded-2xl bg-dark-gray p-3 font-S-CoreDream-200 text-base"
+        placeholder="제목을 입력해주세요"
+      />
       <TextEditorContainer>
         <div className="relative z-[1]">
           <ToolbarPlugin />
