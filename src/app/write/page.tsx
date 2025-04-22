f36eeffecaf328b42124d3da7ad26e687e46b7f5
alwjrqns <yyb02006@gmail.com>
2025-04-23 06:37:45 +0900

    Feat : 텍스트 에디터 제목요소 추가

--- src/app/write/page.tsx
+++ src/app/write/page.tsx
@@ -4,6 +4,7 @@ export default function Write() {
   return (
     <div className="flex h-full justify-center">
       <div className="max-w-[800px] grow space-y-10 py-10">
+        <h1 className="font-patrick-hand text-4xl">Create Memo</h1>
         <Editor />
       </div>
     </div>
