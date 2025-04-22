a0f9708f2951696b0cf174ebc3b2eaee1362fde3
alwjrqns <yyb02006@gmail.com>
2025-04-23 07:12:41 +0900

    Style : ring 클래스를 border로 변경

--- src/components/searchForm.tsx
+++ src/components/searchForm.tsx
@@ -1,6 +1,8 @@
 import { SyntheticEvent, useState } from 'react'
 import { Search } from '../../public/icons/ui'
 import { TextInput } from '#/components/Inputs'
+import { cls } from '#/libs/client/utils'
+import { borderStylesByTheme } from '#/libs/client/constants'
 
 interface SearchFormProps {
   onSearch: (e: SyntheticEvent<HTMLFormElement>) => void
@@ -11,13 +13,13 @@ export default function SearchForm({ onSearch, themeMode }: SearchFormProps) {
   const [searchWord, setSearchWord] = useState('')
   return (
     <form onSubmit={onSearch} className="flex h-14 w-full space-x-2">
-      <div className="flex w-full rounded-full bg-dark-gray ring-1 ring-gray">
+      <div className={cls('flex w-full rounded-full border', borderStylesByTheme[themeMode])}>
         <button className="flex items-center p-2">
           <div className="mr-1 flex aspect-square h-full items-center justify-center rounded-full border border-gray">
             <Search width={20} height={20} strokeColor="#eaeaea" strokeWidth={3} />
           </div>
           <div className="flex items-center">
-            <div className="mt-[2px] border-4 border-transparent border-t-smooth-white"></div>
+            <div className="mt-[2px] border-4 border-transparent border-t-smooth-white" />
           </div>
         </button>
         <TextInput
