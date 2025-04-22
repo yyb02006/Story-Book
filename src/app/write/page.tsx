c62f5b2f400fcc804b0e7345c1cfc2c2375e5c49
alwjrqns <yyb02006@gmail.com>
2025-04-22 23:59:37 +0900

    Style : 텍스트에디터 패딩 추가

--- src/app/write/page.tsx
+++ src/app/write/page.tsx
@@ -3,7 +3,7 @@ import { Editor } from '#/components/editor'
 export default function Write() {
   return (
     <div className="flex h-full justify-center">
-      <div className="max-w-[800px] grow">
+      <div className="max-w-[800px] grow space-y-10 py-10">
         <Editor />
       </div>
     </div>
