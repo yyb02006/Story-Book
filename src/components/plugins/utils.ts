33bfa317444ae84b6477abff26ce43bf34fe25bd
alwjrqns <yyb02006@gmail.com>
2025-04-25 23:47:36 +0900

    Docs : 플러그인 유틸함수에 대한 JSdoc 주석 추가

--- src/components/plugins/utils.ts
+++ src/components/plugins/utils.ts
@@ -3,6 +3,13 @@ import { $setBlocksType } from '@lexical/selection'
 import { BlockType, HeadingNode } from '#/components/plugins/blockTypes'
 import { $createHeadingNode } from '@lexical/rich-text'
 
+/**
+ * 파라미터로 받은 에디터에서 선택된 텍스트 블록을 paragraph로 변환
+ *
+ * @param {LexicalEditor} editor - Lexical 에디터 인스턴스
+ * @example
+ * formatParagraph(editor)
+ */
 export const formatParagraph = (editor: LexicalEditor) => {
   editor.update(() => {
     const selection = $getSelection()
@@ -10,6 +17,16 @@ export const formatParagraph = (editor: LexicalEditor) => {
   })
 }
 
+/**
+ * 파라미터로 받은 에디터에서 선택된 텍스트 블록을 지정된 헤딩 노드 타입으로 변환하는 함수를 반환
+ *
+ * @param {LexicalEditor} editor - Lexical 에디터 인스턴스
+ * @param {BlockType} selectedBlockType - 현재 선택된 블록 타입
+ * @returns {(headingNodeType: HeadingNode) => void} - 헤딩 노드 타입을 인자로 받아 해당 타입으로 블록을 변환하는 함수
+ * @example
+ * const formatHeading = createFormatHeading(editor, 'paragraph')
+ * formatHeading('h1')
+ */
 export const createFormatHeading = (editor: LexicalEditor, selectedBlockType: BlockType) => {
   return (headingNodeType: HeadingNode) => {
     if (selectedBlockType === headingNodeType) return
