89a5c8434e3df52ea34c077ca9bcdbf2b0a0fff5
alwjrqns <yyb02006@gmail.com>
2025-04-26 17:35:26 +0900

    Feat : createFormatQuote 함수 추가 및 클로저 파라미터 이름 수정
    - HeadingNode => HeadingNodeType

--- src/components/plugins/Buttons/quoteButton.tsx
+++ src/components/plugins/Buttons/quoteButton.tsx
@@ -1,34 +1,21 @@
 import { quoteNode } from '#/components/plugins/blockTypes'
-import { $createQuoteNode } from '@lexical/rich-text'
-import { $getSelection, $isRangeSelection } from 'lexical'
-import { $setBlocksType } from '@lexical/selection'
 import { CommonToolButtonProps } from '#/components/plugins/Buttons/buttonTypes'
 import BaseToolButton from '#/components/plugins/Buttons/baseToolButton'
+import { createFormatQuote } from '#/components/plugins/utils'
 
 export default function QuoteButton({
   selectedBlockType,
   editor,
   buttonSize,
 }: CommonToolButtonProps) {
-  const createQuote = () => {
-    if (selectedBlockType !== quoteNode) {
-      editor.update(() => {
-        const selection = $getSelection()
-        if ($isRangeSelection(selection)) {
-          $setBlocksType(selection, () => $createQuoteNode())
-        }
-      })
-    }
-  }
+  const formatQuote = createFormatQuote(editor, selectedBlockType)
 
   return (
     <BaseToolButton
       selectedBlockType={selectedBlockType}
       buttonBlockType={quoteNode}
       buttonSize={buttonSize}
-      onClick={() => {
-        createQuote()
-      }}
+      onClick={formatQuote}
     />
   )
 }
--- src/components/plugins/utils.ts
+++ src/components/plugins/utils.ts
@@ -1,7 +1,7 @@
 import { $createParagraphNode, $getSelection, $isRangeSelection, LexicalEditor } from 'lexical'
 import { $setBlocksType } from '@lexical/selection'
-import { BlockType, HeadingNode } from '#/components/plugins/blockTypes'
-import { $createHeadingNode } from '@lexical/rich-text'
+import { BlockType, HeadingNodeType, quoteNode } from '#/components/plugins/blockTypes'
+import { $createHeadingNode, $createQuoteNode } from '@lexical/rich-text'
 
 /**
  * 파라미터로 받은 에디터에서 선택된 텍스트 블록을 paragraph로 변환
@@ -22,13 +22,13 @@ export const formatParagraph = (editor: LexicalEditor) => {
  *
  * @param {LexicalEditor} editor - Lexical 에디터 인스턴스
  * @param {BlockType} selectedBlockType - 현재 선택된 블록 타입
- * @returns {(headingNodeType: HeadingNode) => void} - 헤딩 노드 타입을 인자로 받아 해당 타입으로 블록을 변환하는 함수
+ * @returns {(headingNodeType: HeadingNodeType) => void} - 헤딩 노드 타입을 인자로 받아 헤딩 노드 타입으로 블록을 변환하는 함수
  * @example
  * const formatHeading = createFormatHeading(editor, 'paragraph')
  * formatHeading('h1')
  */
 export const createFormatHeading = (editor: LexicalEditor, selectedBlockType: BlockType) => {
-  return (headingNodeType: HeadingNode) => {
+  return (headingNodeType: HeadingNodeType) => {
     if (selectedBlockType === headingNodeType) return
     editor.update(() => {
       const selection = $getSelection()
@@ -36,5 +36,28 @@ export const createFormatHeading = (editor: LexicalEditor, selectedBlockType: Bl
         $setBlocksType(selection, () => $createHeadingNode(headingNodeType))
       }
     })
+  }
+}
+
+/**
+ * 파라미터로 받은 에디터에서 선택된 텍스트 블록을 지정된 쿼트 노드 타입으로 변환하는 함수를 반환
+ *
+ * @param {LexicalEditor} editor - Lexical 에디터 인스턴스
+ * @param {BlockType} selectedBlockType - 현재 선택된 블록 타입
+ * @returns {(headingNodeType: QuoteNodeType) => void} - 헤딩 노드 타입을 인자로 받아 쿼트 노드 타입으로 블록을 변환하는 함수
+ * @example
+ * const formatQuote = createFormatQuote(editor, 'paragraph')
+ * formatQuote('h1')
+ */
+export const createFormatQuote = (editor: LexicalEditor, selectedBlockType: BlockType) => {
+  return () => {
+    if (selectedBlockType !== quoteNode) {
+      editor.update(() => {
+        const selection = $getSelection()
+        if ($isRangeSelection(selection)) {
+          $setBlocksType(selection, () => $createQuoteNode())
+        }
+      })
+    }
   }
 }
