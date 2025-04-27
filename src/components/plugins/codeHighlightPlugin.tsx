3ab63aaa913a406ed32cef02c459cdcb41def521
alwjrqns <yyb02006@gmail.com>
2025-04-28 05:55:35 +0900

    Feat : 코드블록 하이라이팅 언어 변경을 위한 레지스터 함수 추가

--- src/components/plugins/codeHighlightPlugin.tsx
+++ src/components/plugins/codeHighlightPlugin.tsx
@@ -1,12 +1,43 @@
 import { useEffect } from 'react'
 import { useLexicalComposerContext } from '@lexical/react/LexicalComposerContext'
-import { registerCodeHighlighting } from '@lexical/code'
+import { registerCodeHighlighting, $isCodeNode, CodeNode } from '@lexical/code'
+import {
+  $getSelection,
+  $isRangeSelection,
+  COMMAND_PRIORITY_CRITICAL,
+  LexicalEditor,
+  createCommand,
+} from 'lexical'
+import { $getNearestNodeOfType, mergeRegister } from '@lexical/utils'
+
+export const CODE_LANGUAGE_COMMAND = createCommand<string>()
+
+const registerCodeLanguageSelecting = (editor: LexicalEditor) => {
+  return editor.registerCommand(
+    CODE_LANGUAGE_COMMAND,
+    (language, editor) => {
+      const selection = $getSelection()
+      if (!$isRangeSelection(selection)) return false
+      const ancherNode = selection.anchor.getNode()
+      const targetNode = $isCodeNode(ancherNode)
+        ? ancherNode
+        : $getNearestNodeOfType(ancherNode, CodeNode) // CodeNode가 자식 노드를 가질 수 있나? TextNode?
+      if (!targetNode) return false
+
+      editor.update(() => {
+        targetNode.setLanguage(language)
+      })
+      return true
+    },
+    COMMAND_PRIORITY_CRITICAL,
+  )
+}
 
 export default function CodeHighlightPlugin(): JSX.Element | null {
   const [editor] = useLexicalComposerContext()
 
   useEffect(() => {
-    return registerCodeHighlighting(editor)
+    return mergeRegister(registerCodeHighlighting(editor), registerCodeLanguageSelecting(editor))
   }, [editor])
 
   return null
