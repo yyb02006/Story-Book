786f04f7f984bf32d24420711bdc3192d9d9ad75
alwjrqns <yyb02006@gmail.com>
2025-04-29 19:57:08 +0900

    Feat : 코드블럭에 대한 구현 추가
    - 언어를 선택할 수 있는 드랍다운 별도 구현
    - 특히 useEffect로직에 대해 추후 상태관리 라이브러리 도입시 리팩토링이 필요함

--- src/components/plugins/Buttons/codeButton.tsx
+++ src/components/plugins/Buttons/codeButton.tsx
@@ -1,21 +1,125 @@
 import { codeNode } from '#/components/plugins/blockTypes'
 import BaseToolButton from '#/components/plugins/Buttons/baseToolButton'
-import { CommonToolButtonProps } from '#/components/plugins/Buttons/buttonTypes'
+import { buttonSizes, CommonToolButtonProps } from '#/components/plugins/Buttons/buttonTypes'
+import ToolbarIcon from '#/components/plugins/Buttons/toolbarIcon'
+import { CODE_LANGUAGE_COMMAND } from '#/components/plugins/codeHighlightPlugin'
 import { formatCode } from '#/components/plugins/utils'
+import { cls } from '#/libs/client/utils'
+import { CODE_LANGUAGE_FRIENDLY_NAME_MAP } from '@lexical/code'
+import { $getSelection, $isRangeSelection } from 'lexical'
+import { Dispatch, SetStateAction, useEffect, useState } from 'react'
+import { $isCodeNode } from '@lexical/code'
+
+const codeLanguagesOptions = Object.entries(CODE_LANGUAGE_FRIENDLY_NAME_MAP).map(
+  ([value, label]) => ({ value, label }),
+)
+
+interface CodeDropDownProps extends Omit<CommonToolButtonProps, 'buttonSize' | 'themeMode'> {
+  setIsDropdownListOpen: Dispatch<SetStateAction<boolean>>
+  isDropdownListOpen: boolean
+  codeLanguage: string
+  languageClickHandler: (value: string) => void
+}
+
+const CodeDropDown = ({
+  selectedBlockType,
+  codeLanguage,
+  isDropdownListOpen,
+  languageClickHandler,
+  setIsDropdownListOpen,
+}: CodeDropDownProps) => {
+  const isLanguageSelected = (value: string) => {
+    // formatCode로 코드 블럭을 만들었을 때 language의 기본값은 javascript임
+    // 그러나 codeLanguagesOptions에는 javascript가 아닌 js를 사용함
+    // javascript !== js 값 불일치 발생 => 현재 선택된 리스트에 제대로 반영되지 못함
+    // 거시기 렉시컬 레포지토리에 PR 올려봐야할듯
+    const normalizedLanguage = codeLanguage === 'javascript' ? 'js' : codeLanguage
+    return normalizedLanguage === value && selectedBlockType === 'code'
+  }
+  return (
+    <div className="relative">
+      <button
+        onClick={() => {
+          setIsDropdownListOpen((p) => !p)
+        }}
+      >
+        <ToolbarIcon className="text-[#808080]" size={buttonSizes.xs} svgId="chevron-down" />
+      </button>
+      {isDropdownListOpen ? (
+        <ul className="bg-charcoal-gray absolute left-0 h-[200px] overflow-hidden rounded-lg">
+          <div className="scrollbar scrollbar-w-1 scrollbar-thumb-bright-blue h-full overflow-y-scroll">
+            {codeLanguagesOptions.map(({ label, value }) => (
+              <li
+                key={value}
+                onClick={() => {
+                  languageClickHandler(value)
+                }}
+                className={cls(
+                  isLanguageSelected(value) ? 'text-bright-blue' : '',
+                  'hover:bg-midnight-gray cursor-pointer px-2 py-1 whitespace-nowrap nth-[1]:pt-2 nth-last-[1]:pb-2',
+                )}
+              >
+                {label}
+              </li>
+            ))}
+          </div>
+        </ul>
+      ) : null}
+    </div>
+  )
+}
 
 export default function CodeButton({
   selectedBlockType,
   editor,
   buttonSize,
 }: CommonToolButtonProps) {
+  const [isDropdownListOpen, setIsDropdownListOpen] = useState<boolean>(false)
+  const [codeLanguage, setCodeLanguage] = useState('')
+
+  useEffect(() => {
+    return editor.registerUpdateListener(({ editorState }) => {
+      editorState.read(() => {
+        const selection = $getSelection()
+        if (!$isRangeSelection(selection)) return
+        const anchorNode = selection.anchor.getNode()
+        const targetNode =
+          anchorNode.getKey() === 'root' ? anchorNode : anchorNode.getTopLevelElementOrThrow()
+        if ($isCodeNode(targetNode)) {
+          const language = targetNode.getLanguage()
+          if (!language) return
+          setCodeLanguage(language)
+        }
+      })
+    })
+  }, [editor])
+
+  const handleLanguageClick = (value: string) => {
+    editor.dispatchCommand(CODE_LANGUAGE_COMMAND, value)
+    if (selectedBlockType !== 'code') {
+      formatCode(editor, selectedBlockType)
+    }
+    setIsDropdownListOpen(false)
+  }
+
   return (
-    <BaseToolButton
-      selectedBlockType={selectedBlockType}
-      buttonBlockType={codeNode}
-      buttonSize={buttonSize}
-      onClick={() => {
-        formatCode(editor, selectedBlockType)
-      }}
-    />
+    <div className="relative flex items-center">
+      <BaseToolButton
+        selectedBlockType={selectedBlockType}
+        buttonBlockType={codeNode}
+        buttonSize={buttonSize}
+        onClick={() => {
+          formatCode(editor, selectedBlockType)
+        }}
+      />
+      <CodeDropDown
+        languageClickHandler={handleLanguageClick}
+        codeLanguage={codeLanguage}
+        isDropdownListOpen={isDropdownListOpen}
+        setIsDropdownListOpen={setIsDropdownListOpen}
+        editor={editor}
+        selectedBlockType={selectedBlockType}
+      />
+    </div>
   )
 }
