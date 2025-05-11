d9b890b6dd256b8d185cffcd8d03f6fb29a81bac
alwjrqns <yyb02006@gmail.com>
2025-05-11 10:05:02 +0900

    Feat : 링크 버튼 추가 및 관련 상태 추가

--- src/components/plugins/inlineToolbarPlugin.tsx
+++ src/components/plugins/inlineToolbarPlugin.tsx
@@ -2,8 +2,11 @@ import { buttonSizes } from '#/components/plugins/Buttons/buttonTypes'
 import ToolbarIcon from '#/components/plugins/Buttons/toolbarIcon'
 import { useLexicalComposerContext } from '@lexical/react/LexicalComposerContext'
 import { $getSelection, $isRangeSelection, FORMAT_TEXT_COMMAND } from 'lexical'
-import { useEffect, useMemo, useState } from 'react'
+import { Dispatch, SetStateAction, useEffect, useMemo, useState } from 'react'
 import { $isCodeNode } from '@lexical/code'
+import { TOGGLE_LINK_COMMAND, $isLinkNode } from '@lexical/link'
+import { cls, sanitizeUrl } from '#/libs/client/utils'
+import { getSelectedNode } from '#/components/plugins/utils'
 
 const SupportedInlineTypes = [
   'code',
@@ -15,7 +18,11 @@ const SupportedInlineTypes = [
   'subscript',
 ] as const
 
-export default function InlineToolbarPlugin() {
+export default function InlineToolbarPlugin({
+  setIsLinkEditMode,
+}: {
+  setIsLinkEditMode: Dispatch<SetStateAction<boolean>>
+}) {
   const [editor] = useLexicalComposerContext()
 
   const initialOnTextFormatState = useMemo(() => {
@@ -31,6 +38,7 @@ export default function InlineToolbarPlugin() {
   }, [])
 
   const [onTextFormats, setOnTextFormats] = useState(initialOnTextFormatState)
+  const [onLink, setOnLink] = useState(false)
 
   useEffect(() => {
     return editor.registerUpdateListener(({ editorState }) => {
@@ -39,9 +47,16 @@ export default function InlineToolbarPlugin() {
 
         if (!$isRangeSelection(selection)) return
 
-        const anchorNode = selection.anchor.getNode()
+        const anchorNode = getSelectedNode(selection)
         const targetNode =
           anchorNode.getKey() === 'root' ? anchorNode : anchorNode.getTopLevelElementOrThrow()
+
+        // link 업데이트 우선 수행
+        // const node = getSelectedNode(selection)
+
+        const parent = anchorNode.getParent()
+        const isLink = $isLinkNode(parent) || $isLinkNode(anchorNode) // 앵커 노드가 link일 수가 있나?
+        setOnLink(isLink)
 
         // 새로운 객체를 계속 생성하는데, 이게 성능적으로 좋을까?
         const newTextFormats = SupportedInlineTypes.reduce(
@@ -70,6 +85,16 @@ export default function InlineToolbarPlugin() {
     })
   }, [editor])
 
+  const insertLinkHandler = () => {
+    if (!onLink) {
+      setIsLinkEditMode(true)
+      editor.dispatchCommand(TOGGLE_LINK_COMMAND, sanitizeUrl('https://'))
+    } else {
+      setIsLinkEditMode(false)
+      editor.dispatchCommand(TOGGLE_LINK_COMMAND, null)
+    }
+  }
+
   return (
     <div className="flex space-x-3">
       {SupportedInlineTypes.map((inlineType) => {
@@ -86,15 +111,32 @@ export default function InlineToolbarPlugin() {
             <ToolbarIcon
               svgId={`text-${inlineType}`}
               size={buttonSizes['md']}
-              className={
+              className={cls(
+                'hover:text-bright-blue',
                 onTextFormats[inlineType]
                   ? 'text-bright-blue'
-                  : 'dark:text-dark-disabled-icon text-light-disabled-icon'
-              }
+                  : 'dark:text-dark-disabled-icon text-light-disabled-icon',
+              )}
             />
           </button>
         )
       })}
+      <button
+        onClick={insertLinkHandler}
+        aria-label="Insert link"
+        title={`Insert link`}
+        type="button"
+        className={onLink ? 'text-amber-400' : ''}
+      >
+        <ToolbarIcon
+          svgId={'link'}
+          size={buttonSizes['md']}
+          className={cls(
+            'hover:text-bright-blue',
+            onLink ? 'text-bright-blue' : 'dark:text-dark-disabled-icon text-light-disabled-icon',
+          )}
+        />
+      </button>
     </div>
   )
 }
