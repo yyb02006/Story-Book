f55b8c63a456420c10db05d830ed8f1555c3c392
alwjrqns <yyb02006@gmail.com>
2025-05-30 19:20:10 +0900

    Feat : modal ui 수정

--- src/components/modal.tsx
+++ src/components/modal.tsx
@@ -1,3 +1,5 @@
+import ToolbarIcon from '#/lexical/components/Buttons/toolbarIcon'
+
 interface ModalProps {
   onClose: () => void
   title: string
@@ -11,18 +13,26 @@ export default function Modal({ onClose, title, closeOnClickOutside, children }:
   }
   return (
     <div
-      className="fixed h-screen w-screen bg-black/40"
+      className="font-S-CoreDream-500 fixed top-0 left-0 flex h-screen w-screen flex-col items-center justify-center bg-black/40 pb-10"
       onClick={() => {
         if (closeOnClickOutside) {
           onCloseClick()
         }
       }}
     >
-      <div>
-        <span>{title}</span>
-        <button onClick={onCloseClick}>Close</button>
+      <div className="bg-midnight-gray space-y-4 rounded-md p-3">
+        <div className="flex min-w-full items-center justify-between gap-x-6">
+          <span>{title}</span>
+          <button onClick={onCloseClick} className="">
+            <ToolbarIcon
+              size="size-[24px]"
+              svgId="cancel"
+              className="dark:text-dark-disabled-icon text-light-disabled-icon hover:text-bright-blue"
+            />
+          </button>
+        </div>
+        <div>{children}</div>
       </div>
-      {children}
     </div>
   )
 }
