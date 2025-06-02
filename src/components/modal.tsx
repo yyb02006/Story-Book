f72cd12503154306ff022b02c8e9b51a7b162144
alwjrqns <yyb02006@gmail.com>
2025-06-02 19:59:38 +0900

    Feat : 제목이 존재하지 않는 모달에 대한 ui 추가

--- src/components/modal.tsx
+++ src/components/modal.tsx
@@ -1,4 +1,5 @@
 import ToolbarIcon from '#/lexical/components/Buttons/toolbarIcon'
+import { cls } from '#/libs/client/utils'
 
 interface ModalProps {
   onClose: () => void
@@ -13,26 +14,38 @@ export default function Modal({ onClose, title, closeOnClickOutside, children }:
   }
   return (
     <div
-      className="font-S-CoreDream-500 fixed top-0 left-0 flex h-screen w-screen flex-col items-center justify-center bg-black/40 pb-10"
+      className={cls(
+        closeOnClickOutside ? 'cursor-pointer' : '',
+        'font-S-CoreDream-500 fixed top-0 left-0 z-[1000] flex h-screen w-screen flex-col items-center justify-center bg-black/40 pb-10',
+      )}
       onClick={() => {
         if (closeOnClickOutside) {
           onCloseClick()
         }
       }}
     >
-      <div className="bg-midnight-gray space-y-4 rounded-md p-3">
-        <div className="flex min-w-full items-center justify-between gap-x-6">
-          <span>{title}</span>
-          <button onClick={onCloseClick} className="">
-            <ToolbarIcon
-              size="size-[24px]"
-              svgId="cancel"
-              className="dark:text-dark-disabled-icon text-light-disabled-icon hover:text-bright-blue"
-            />
-          </button>
+      {title.length > 0 ? (
+        <div
+          className="bg-midnight-gray cursor-default space-y-4 rounded-md p-3"
+          onClick={(event) => {
+            event.stopPropagation()
+          }}
+        >
+          <div className="flex min-w-full items-center justify-between gap-x-6">
+            <span>{title}</span>
+            <button onClick={onCloseClick} className="">
+              <ToolbarIcon
+                size="size-[24px]"
+                svgId="cancel"
+                className="dark:text-dark-disabled-icon text-light-disabled-icon hover:text-bright-blue"
+              />
+            </button>
+          </div>
+          <div>{children}</div>
         </div>
-        <div>{children}</div>
-      </div>
+      ) : (
+        <div className="overflow-hidden rounded-md">{children}</div>
+      )}
     </div>
   )
 }
