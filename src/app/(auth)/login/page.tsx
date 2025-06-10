566ba04d726a879c6477093778bd81c848fa1d13
alwjrqns <yyb02006@gmail.com>
2025-06-10 16:56:51 +0900

    Feat : 서버액션 파일 위치 변경 및 login 페이지 링크 경로 수정

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,8 +1,9 @@
 'use client'
 
-import { handleForm } from '#/app/(auth)/login/actions'
+import { handleForm } from '#/app/(auth)/actions'
 import FormButton from '#/components/form/formButton'
 import FormTextInput from '#/components/form/formTextInput'
+import SNSAuth from '#/components/SNSAuth'
 import Link from 'next/link'
 import { useFormState } from 'react-dom'
 
@@ -12,26 +13,7 @@ export default function Login() {
     <div className="flex h-screen w-screen flex-col items-center">
       <section className="flex h-full w-[400px] flex-col justify-center">
         <h1 className="font-patrick mb-5 flex justify-center text-4xl font-bold">MEMOISM</h1>
-        <div className="flex flex-col space-y-4">
-          <button className="bg-smooth-white text-smooth-black font-S-CoreDream-400 flex h-10 items-center justify-center space-x-1 rounded-lg text-sm">
-            <svg className="size-6">
-              <use href={`/icons/brandIcons.svg#google`} />
-            </svg>
-            <span>구글 로그인</span>
-          </button>
-          <button className="bg-smooth-white text-smooth-black font-S-CoreDream-400 flex h-10 items-center justify-center space-x-1 rounded-lg text-sm">
-            <svg className="size-6">
-              <use href={`/icons/brandIcons.svg#github`} />
-            </svg>
-            <span>깃헙 로그인</span>
-          </button>
-          <button className="text-smooth-black font-S-CoreDream-400 flex h-10 items-center justify-center space-x-2 rounded-lg bg-[#FEE500] text-sm">
-            <svg className="size-4">
-              <use href={`/icons/brandIcons.svg#kakao`} />
-            </svg>
-            <span>카카오 로그인</span>
-          </button>
-        </div>
+        <SNSAuth buttonText="로그인" />
         <div className="flex w-full items-center space-x-4">
           <div className="bg-charcoal-gray my-6 h-[1px] w-full rounded-full"></div>
           <div className="font-S-CoreDream-400 text-light-gray text-xs break-keep">또는</div>
@@ -55,7 +37,7 @@ export default function Login() {
         </form>
         <div className="font-S-CoreDream-200 text-white-gray mt-2 flex w-full items-center justify-evenly px-4 text-xs">
           <button className="hover:text-bright-blue">
-            <Link href={'/login'}>회원가입</Link>
+            <Link href={'/signup'}>회원가입</Link>
           </button>
           <div className="bg-white-gray h-4/5 w-[1px]" />
           <button className="hover:text-bright-blue">
@@ -63,7 +45,7 @@ export default function Login() {
           </button>
           <div className="bg-white-gray h-4/5 w-[1px]" />
           <button className="hover:text-bright-blue">
-            <Link href={'/login'}>비밀번호 찾기</Link>
+            <Link href={'/find/password'}>비밀번호 찾기</Link>
           </button>
         </div>
       </section>
