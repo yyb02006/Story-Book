68b5da246d109ce0cb193c2c736bef3eee107bca
alwjrqns <yyb02006@gmail.com>
2025-06-12 20:06:39 +0900

    Style : input 관련 global css와 baseInput의 유틸리티 클래스 정리

--- src/components/Inputs/baseInput.tsx
+++ src/components/Inputs/baseInput.tsx
@@ -1,13 +1,11 @@
 'use client'
 
 import { cls } from '#/libs/client/utils'
-import { ChangeEvent, InputHTMLAttributes } from 'react'
+import { InputHTMLAttributes } from 'react'
 
 interface InputProps {
   name: string
   className?: string
-  onChange?: (event: ChangeEvent<HTMLInputElement>) => void
-  placeholder?: string
 }
 
 const BaseInput = ({
@@ -15,13 +13,13 @@ const BaseInput = ({
   className = '',
   ...rest
 }: InputProps & InputHTMLAttributes<HTMLInputElement>) => {
-  const baseInputStyles = 'w-full placeholder:pl-1 focus:ring-0 font-S-CoreDream-400'
+  const baseInputStyles = 'w-full placeholder:pl-1 font-S-CoreDream-400'
 
   return (
     <input
       name={name}
       spellCheck={false}
-      className={cls(baseInputStyles, className)}
+      className={cls(className, baseInputStyles)}
       // size={20} default
       {...rest}
     />
