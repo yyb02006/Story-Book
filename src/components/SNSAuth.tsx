f12bf135b6553584e118539e6a14f615ed9a1cb2
alwjrqns <yyb02006@gmail.com>
2025-06-18 23:04:08 +0900

    Feat : Link href 전달 함수 추가

--- src/components/SNSAuth.tsx
+++ src/components/SNSAuth.tsx
@@ -1,10 +1,21 @@
 'use client'
 
+import Link from 'next/link'
+
 interface SNSAuthProps {
   buttonText: string
 }
 
+const getSNSAuthURL = (baseURL: string, params?: { [key: string]: string }) => {
+  const formattedParams = new URLSearchParams(params).toString()
+  return `${baseURL}?${formattedParams}`
+}
+
 export default function SNSAuth({ buttonText }: SNSAuthProps) {
+  const githubAuthURL = getSNSAuthURL('https://github.com/login/oauth/authorize', {
+    client_id: process.env.NEXT_PUBLIC_GITHUB_CLIENT_ID!,
+    scope: 'read:user,user:email',
+  })
   return (
     <div className="flex flex-col space-y-4">
       <button className="bg-smooth-white text-smooth-black font-S-CoreDream-400 flex h-10 items-center justify-center space-x-1 rounded-lg text-sm">
@@ -13,12 +24,17 @@ export default function SNSAuth({ buttonText }: SNSAuthProps) {
         </svg>
         <span>구글 {buttonText}</span>
       </button>
-      <button className="bg-smooth-white text-smooth-black font-S-CoreDream-400 flex h-10 items-center justify-center space-x-1 rounded-lg text-sm">
+
+      <Link
+        href={githubAuthURL}
+        className="bg-smooth-white hover:ring-bright-blue text-smooth-black font-S-CoreDream-400 flex h-10 items-center justify-center space-x-1 overflow-hidden rounded-lg text-sm hover:ring-2"
+      >
         <svg className="size-6">
           <use href={`/icons/brandIcons.svg#github`} />
         </svg>
         <span>깃헙 {buttonText}</span>
-      </button>
+      </Link>
+
       <button className="text-smooth-black font-S-CoreDream-400 flex h-10 items-center justify-center space-x-2 rounded-lg bg-[#FEE500] text-sm">
         <svg className="size-4">
           <use href={`/icons/brandIcons.svg#kakao`} />
