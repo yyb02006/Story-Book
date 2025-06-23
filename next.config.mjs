515c84321578c0cb43533fd94446b4f61de06c5d
alwjrqns <yyb02006@gmail.com>
2025-06-23 21:49:41 +0900

    Chore : 이미지 컴포넌트 허용 경로 패턴 추가

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,19 @@
 import NextBundleAnalyzer from '@next/bundle-analyzer'
 
 /** @type {import('next').NextConfig} */
-const nextConfig = { reactStrictMode: true }
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'k.kakaocdn.net',
+        port: '',
+        pathname: '**',
+      },
+    ],
+  },
+}
 
 const withBundleAnalyzer = NextBundleAnalyzer({
   enabled: process.env.ANALYZE === 'true',
