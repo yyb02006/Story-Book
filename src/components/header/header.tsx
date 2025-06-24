cb62cc83f50d005ea1ba0b369000141da9e8c584
alwjrqns <yyb02006@gmail.com>
2025-06-24 21:07:35 +0900

    Feat : 서버 컴포넌트로 변경 및 getUser함수 추가, userMenu 컴포넌트 추가

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,21 +1,38 @@
-'use client'
-
+import UserMenu from '#/components/header/userMenu'
 import SearchForm from '#/components/searchForm'
+import prisma from '#/libs/server/prisma'
+import getSession from '#/libs/server/session'
 import Link from 'next/link'
 
-export default function Header() {
+const getUser = async () => {
+  const session = await getSession()
+  if (session.id) {
+    const user = await prisma.user.findUnique({
+      where: { id: session.id },
+      select: { username: true, avatar: true },
+    })
+    return user
+  }
+}
+
+export default async function Header() {
+  const user = await getUser()
   return (
     <div className="dark:bg-smooth-black bg-smooth-white dark:border-dark-border border-light-border pl-gnb-left absolute top-0 left-0 z-[1000] flex w-full justify-center border-b px-4 py-2">
       <div className="w-full max-w-[500px]">
-        <SearchForm onSearch={() => {}} />
+        <SearchForm />
       </div>
       <div className="font-S-CoreDream-500 absolute top-0 right-0 flex h-full items-center justify-center py-3 pr-4">
-        <Link
-          className="bg-bright-blue flex h-full items-center justify-center rounded-full px-4 text-sm"
-          href="/login"
-        >
-          Log In
-        </Link>
+        {user ? (
+          <UserMenu {...user} />
+        ) : (
+          <Link
+            className="bg-bright-blue flex h-full items-center justify-center rounded-full px-4 text-sm"
+            href="/login"
+          >
+            Log In
+          </Link>
+        )}
       </div>
     </div>
   )
