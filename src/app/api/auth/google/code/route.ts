6eecf1fb18725794c64582136a73c7b57af0ce2c
alwjrqns <yyb02006@gmail.com>
2025-06-24 21:05:31 +0900

    Feat : scope에 전달되는 문자열 url 형식 수정

--- src/app/api/auth/google/code/route.ts
+++ src/app/api/auth/google/code/route.ts
@@ -1,11 +1,15 @@
+import { cls } from '#/libs/client/utils'
 import { getSNSAuthURL } from '#/libs/server/utils'
 import { redirect } from 'next/navigation'
 
 const googleAuthURL = getSNSAuthURL(`https://accounts.google.com/o/oauth2/v2/auth`, {
   client_id: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID!,
   response_type: 'code',
-  scope: encodeURIComponent('profile email'),
-  redirect_uri: 'http://localhost:3000/api/google/token',
+  scope: cls(
+    'https://www.googleapis.com/auth/userinfo.profile',
+    'https://www.googleapis.com/auth/userinfo.email',
+  ),
+  redirect_uri: 'http://localhost:3000/api/auth/google',
 })
 
 export async function GET() {
