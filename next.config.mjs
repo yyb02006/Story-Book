a5e0a52bcce398ae6a5a601d25c46e5336cdb316
alwjrqns <yyb02006@gmail.com>
2025-06-24 20:58:56 +0900

    Chore : 구글, 깃헙 프로필 이미지 주소 remotePatterns 추가

--- next.config.mjs
+++ next.config.mjs
@@ -11,6 +11,18 @@ const nextConfig = {
         port: '',
         pathname: '**',
       },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+        port: '',
+        pathname: '**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'avatars.githubusercontent.com',
+        port: '',
+        pathname: '**',
+      },
     ],
   },
 }
