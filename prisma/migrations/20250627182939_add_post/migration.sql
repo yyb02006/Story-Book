d1109f92f7b3dd3e3b1efbe4dd9d269a01ae0d06
alwjrqns <yyb02006@gmail.com>
2025-06-29 23:59:15 +0900

    Feat : Post model 추가

---
+++ prisma/migrations/20250627182939_add_post/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE "Post" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "title" TEXT NOT NULL,
+    "htmlContent" TEXT NOT NULL,
+    "editorState" JSONB NOT NULL,
+    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" DATETIME NOT NULL,
+    "userId" INTEGER NOT NULL,
+    CONSTRAINT "Post_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
