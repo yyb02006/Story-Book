11acd678e54f83677899bd03f92532997b588568
alwjrqns <yyb02006@gmail.com>
2025-06-30 21:11:41 +0900

    Feat : main 페이지에 사용자 post list 추가
    - 캬 이맛에 서버 컴포넌트 쓰는 거 아입니까!

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,3 +1,32 @@
-export default function Home() {
-  return <div className="size-full">notes section</div>
+import prisma from '#/libs/server/prisma'
+import getSession from '#/libs/server/session'
+import sanitizeHtml from 'sanitize-html'
+
+const getPosts = async () => {
+  const session = getSession()
+  if (!session) throw new Error('no user session')
+  return await prisma.post.findMany({
+    select: { created_at: true, htmlContent: true, title: true },
+    take: 10,
+    orderBy: { id: 'desc' },
+  })
+}
+
+export default async function Home() {
+  const posts = await getPosts()
+  return (
+    <div className="flex size-full flex-col items-center">
+      {posts.map((post) => (
+        <div
+          dangerouslySetInnerHTML={{
+            __html: sanitizeHtml(post.htmlContent, {
+              allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img']),
+              allowedSchemes: sanitizeHtml.defaults.allowedSchemes.concat(['data']),
+            }),
+          }}
+          key={post.created_at.getDate()}
+        />
+      ))}
+    </div>
+  )
 }
