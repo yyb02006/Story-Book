ed14d9e02417ae9048189021e5ea9c74419c217d
alwjrqns <yyb02006@gmail.com>
2025-07-13 06:25:34 +0900

    Feat : 컬러피커 고정색상을 위한 상수 추가

---
+++ src/lexical/const.ts
@@ -0,0 +1,34 @@
+export const basicColorGroups = [
+  [
+    { utilityClass: 'bg-[#eaeaea]', colorHex: '#eaeaea' },
+    { utilityClass: 'bg-[#bbbbbb]', colorHex: '#bbbbbb' },
+  ],
+  [
+    { utilityClass: 'bg-[#E53935]', colorHex: '#E53935' },
+    { utilityClass: 'bg-[#FF6F61]', colorHex: '#FF6F61' },
+    { utilityClass: 'bg-[#FB8C00]', colorHex: '#FB8C00' },
+    { utilityClass: 'bg-[#FDD835]', colorHex: '#FDD835' },
+  ],
+  [
+    { utilityClass: 'bg-[#2543f0]', colorHex: '#2543f0' },
+    { utilityClass: 'bg-[#3F51B5]', colorHex: '#3F51B5' },
+    { utilityClass: 'bg-[#1E88E5]', colorHex: '#1E88E5' },
+    { utilityClass: 'bg-[#81D4FA]', colorHex: '#81D4FA' },
+  ],
+  [
+    { utilityClass: 'bg-[#555555]', colorHex: '#555555' },
+    { utilityClass: 'bg-[#101010]', colorHex: '#101010' },
+  ],
+  [
+    { utilityClass: 'bg-[#1fc027]', colorHex: '#1fc027' },
+    { utilityClass: 'bg-[#288f00]', colorHex: '#288f00' },
+    { utilityClass: 'bg-[#7ceb82]', colorHex: '#7ceb82' },
+    { utilityClass: 'bg-[#42dfb0]', colorHex: '#42dfb0' },
+  ],
+  [
+    { utilityClass: 'bg-[#7e5548]', colorHex: '#7e5548' },
+    { utilityClass: 'bg-[#8E24AA]', colorHex: '#8E24AA' },
+    { utilityClass: 'bg-[#9e1442]', colorHex: '#9e1442' },
+    { utilityClass: 'bg-[#6919ff]', colorHex: '#6919ff' },
+  ],
+]
