de2d8f37e947829cad9605ad260e545a1e110f4b
alwjrqns <yyb02006@gmail.com>
2025-07-15 20:06:13 +0900

    Feat : nodes 타입 정확하게 수정 및 extendedTextNode로 기존 TextNode대체

--- src/lexical/nodes/EditorNodes.ts
+++ src/lexical/nodes/EditorNodes.ts
@@ -1,15 +1,24 @@
 import { HeadingNode, QuoteNode } from '@lexical/rich-text'
 import { ListItemNode, ListNode } from '@lexical/list'
-import { Klass, LexicalNode } from 'lexical'
+import { KlassConstructor, LexicalNode, LexicalNodeReplacement, TextNode } from 'lexical'
 import { CodeNode, CodeHighlightNode } from '@lexical/code'
 import { LinkNode } from '@lexical/link'
 import { StickyNode } from '#/lexical/nodes/StickyNode'
 import { ImageNode } from '#/lexical/nodes/ImageNode'
+import { ExtendedTextNode } from '#/lexical/nodes/extendedTextNode'
 
 //
 // Klass<T> : 제네릭 T 타입의 클래스의 인스턴스를 생성할 수 있는 클래스만 허용하는 유틸리티 타입
 //
-export const nodes: Klass<LexicalNode>[] = [
+export const nodes:
+  | readonly (KlassConstructor<typeof LexicalNode> | LexicalNodeReplacement)[]
+  | undefined = [
+  ExtendedTextNode,
+  {
+    replace: TextNode,
+    with: (node: TextNode) => new ExtendedTextNode(node.__text),
+    withKlass: ExtendedTextNode,
+  },
   HeadingNode,
   QuoteNode,
   ListNode,
